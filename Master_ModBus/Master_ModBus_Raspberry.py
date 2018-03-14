2d1b6f9307deb4438409f5b9bada4c9adc5ec77d
thibault-lacroix <32330447+thibault-lacroix@users.noreply.github.com>
2018-03-14 09:06:25 +0100

    Communication avec la base de données ajouté
    Envoi des données dans la base de données, dans la table capteur, les lignes capt_temp et capt_debit

--- Master_ModBus/Master_ModBus_Raspberry.py
+++ Master_ModBus/Master_ModBus_Raspberry.py
@@ -1,14 +1,41 @@
 #!/usr/bin/env python
 import minimalmodbus
 import time
+import MySQLdb
+import datetime
 
-minimalmodbus.BAUDRATE = 19200
+minimalmodbus.BAUDRATE = 19200 #initialisation du baudrate
 
-# port name, slave address (in decimal)
-instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
+instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1) # Nom du port, adresse de l'esclave 
 
 while True:
     # Register number, number of decimals, function code
     temperature = instrument.read_register(1, 2, 4)
-    print temperature
+    print "Temperature: ", temperature
     time.sleep(1)
+    debit = instrument.read_register(2, 2, 4)
+    print "Debit:", debit
+    time.sleep(1)
+
+    #Query de connexion
+    db = MySQLdb.connect("localhost","root","btsir123","ormeaux") 
+
+    cursor = db.cursor()
+    #Query SQL
+    sql = ("""INSERT INTO capteur(capt_temp, capt_debit) VALUES (%s, %s)""") 
+    data = (temperature, debit)
+    print "ok"
+    try:
+        #Execution de la query
+        cursor.execute(sql, data) 
+        print "test"
+        db.commit() 
+        print "commit"
+
+    except:
+        #en cas d'erreur
+        db.rollback() 
+        print "erreur"
+    #fermeture de la connexion avec la base de donnees
+    db.close()
+    print "deconnecte"
