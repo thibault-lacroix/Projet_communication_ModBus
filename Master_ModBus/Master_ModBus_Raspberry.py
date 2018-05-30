e8e95ea95fbdaf30195c80eab0c6240fe4a1e5e6
thibault-lacroix <32330447+thibault-lacroix@users.noreply.github.com>
2018-05-30 17:09:12 +0200

    Ajout envoi vers arduino

--- Master_ModBus/Master_ModBus_Raspberry.py
+++ Master_ModBus/Master_ModBus_Raspberry.py
@@ -5,53 +5,75 @@ import minimalmodbus
 import MySQLdb
 import time
 from settings import *
+import sys
+
+REG_DEBIT = 0
+REG_TEMP = 1
+REG_VANNE_STATE = 2
 
 
+if sys.argv[1] :
+    print "erreur il manque le debit"
+    exit
+    
+else:
+    val_debit=int(sys.argv[1])
+
+print val_debit
+
+print "Adresse de l'esclave: ", slave_addr
+
+instrument = minimalmodbus.Instrument('/dev/ttyUSB0', slave_addr,mode='rtu')
 while True:
 
-    print "Adresse de l'esclave: ", slave_addr
-    time.sleep(1)
-    print "Température: ", temperature,"°C"
-    time.sleep(1)
-    print "Débit: ", debit
-    time.sleep(1)
 
-    db = MySQLdb.connect("localhost","root","btsir123","ormeaux") # Query de connexion
+    debit=instrument.read_register(REG_DEBIT, 0)
+    print "1.Débit: ", debit
 
-    cursor = db.cursor()
-    curseur = db.cursor()
-    idcurs = db.cursor()
+    temperature=instrument.read_register(REG_TEMP, 2)
+    print "2.Température: ", temperature,"°C"
 
-    query = ("SELECT valeurs FROM settings")
+    val_debit = val_debit + 1
+    instrument.write_register(REG_VANNE_STATE, val_debit)
+    print "3.Controle de l'electrovanne: ", val_debit,"L/min"
     
-    sql = ("""INSERT INTO mesure(temp, debit, id_bassin) VALUES (%s, %s, %s)""") # Query SQL
-    data = (temperature, debit, slave_addr)
-    iD = ("SELECT id_mesure FROM mesure WHERE id_bassin=1 ORDER BY datetime DESC LIMIT 1")
 
-    print "Query ok"
-    try:
-        cursor.execute(sql, data) # Execution de la query
-        print "Connexion à la db"
+    # db = MySQLdb.connect("localhost","root","btsir123","ormeaux") # Query de connexion
 
-        curseur.execute(query) 
-        row = curseur.fetchone() # Resultat de la query 
-        valeur = row[0] # Affichage de la première valeur du tableau
+    # cursor = db.cursor()
+    # curseur = db.cursor()
+    # idcurs = db.cursor()
 
-        idcurs.execute(iD)
-        val = idcurs.fetchone()
-        id_mes = val[0]        
+    # query = ("SELECT valeurs FROM settings")
+    
+    # sql = ("""INSERT INTO mesure(temp, debit, id_bassin) VALUES (%s, %s, %s)""") # Query SQL
+    # data = (temperature, debit, slave_addr)
+    # iD = ("SELECT id_mesure FROM mesure WHERE id_bassin=1 ORDER BY datetime DESC LIMIT 1")
 
-        db.commit() 
-        print "Envoi"
+    # print "Query ok"
+    # try:
+    #     cursor.execute(sql, data) # Execution de la query
+    #     print "Connexion à la db"
 
-    except:
-        db.rollback() # En cas d'erreur
-        print "Erreur"
+    #     curseur.execute(query) 
+    #     row = curseur.fetchone() # Resultat de la query 
+    #     valeur = row[0] # Affichage de la première valeur du tableau
 
-    db.close() # Fermeture de la connexion avec la base de donnees
-    print "ID", id_mes+1
-    print "sleep: ",valeur
-    print "Déconnexion de la db"
-    print "--------------------"
-    print "                    "
-    time.sleep(valeur)
+    #     idcurs.execute(iD)
+    #     val = idcurs.fetchone()
+    #     id_mes = val[0]        
+
+    #     db.commit() 
+    #     print "Envoi"
+
+    # except:
+    #     db.rollback() # En cas d'erreur
+    #     print "Erreur"
+
+    # db.close() # Fermeture de la connexion avec la base de donnees
+    # print "ID", id_mes
+    # print "sleep: ",valeur
+    # print "Déconnexion de la db"
+    # print "--------------------"
+    # print "                    "
+    # time.sleep(valeur-3)
--- Master_ModBus/settings.py
+++ Master_ModBus/settings.py
@@ -2,13 +2,8 @@
 # -*- coding: ISO-8859-1 -*-
 
 import minimalmodbus
-import MySQLdb
 
 
 minimalmodbus.BAUDRATE = 19200
-minimalmodbus.TIMEOUT = 60
+minimalmodbus.TIMEOUT = 3
 slave_addr = 1
-instrument = minimalmodbus.Instrument('/dev/ttyUSB0', slave_addr)
-
-temperature = instrument.read_register(1, 2) # Donnée dans le tableau, nombre de décimales
-debit = instrument.read_register(0, 0)
