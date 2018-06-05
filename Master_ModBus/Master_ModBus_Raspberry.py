a4c17f99165e4b1a9ac23e8ffa57a9cca71e1e19
thibault-lacroix <32330447+thibault-lacroix@users.noreply.github.com>
2018-06-05 14:35:36 +0200

    Fonction capture et envoi dans la base de données

--- Master_ModBus/Master_ModBus_Raspberry.py
+++ Master_ModBus/Master_ModBus_Raspberry.py
@@ -7,40 +7,15 @@ import time
 from settings import *
 import sys
 
-REG_DEBIT = 0
-REG_TEMP = 1
-REG_VANNE_STATE = 2
-REG_DEBIT_STATE = 3
+slave_addr = 1
 
-val_electro=1
-val_debit=20
-
-#if sys.argv[1] :
-#    print "erreur il manque le debit"
-#    exit
-    
-#else:
-#    val_electro=int(sys.argv[1])
-
-
-print "Adresse de l'esclave: ", slave_addr
-
-instrument = minimalmodbus.Instrument('/dev/ttyUSB0', slave_addr,mode='rtu')
+instrument = minimalmodbus.Instrument(port, slave_addr,mode='rtu')
 
 while True:
 
     debit=instrument.read_register(REG_DEBIT, 0)
-    print "1.Débit: ", debit
 
     temperature=instrument.read_register(REG_TEMP, 2)
-    print "2.Température: ", temperature,"°C"
-
-    #val_electro = val_electro + 1
-    instrument.write_register(REG_VANNE_STATE, val_electro)
-    print "3.Etat de l'electrovanne:", val_electro
-
-    instrument.write_register(REG_DEBIT_STATE, val_debit)
-    print "4.Valeur du debit: ", val_debit
 
 
     db = MySQLdb.connect("localhost","root","btsir123","ormeaux") # Query de connexion
@@ -51,27 +26,14 @@ while True:
     queryInsertMes = ("""INSERT INTO mesure(temp, debit, id_bassin) VALUES (%s, %s, %s)""") # Query SQL
     data = (temperature, debit, slave_addr)
 
-    debitBassin = ("""SELECT debitentre FROM controldeb WHERE id_bassin=?""")
-
-    print "Query ok"
 
     try:
-        cursor.execute(queryInsertMes, data) # Execution de la query
+        cursor.execute(queryInsertMes, data) # Insertion de la base dans données
 
-        cursDebBass.execute("""SELECT debitentre FROM controldeb WHERE id_bassin=(%s)""", (slave_addr,))
-
-        controlDeb = cursDebBass.fetchone()
-        valControlDeb = controlDeb[0]
-        print "Connexion à la db"
         db.commit() 
-        print "Envoi"
 
     except:
         db.rollback() # En cas d'erreur
-        print "Erreur"
 
     db.close() # Fermeture de la connexion avec la base de donnees
-    print "control debit: ", valControlDeb
-    print "Déconnexion de la db"
-    print "--------------------"
-    print "                    "
+    time.sleep(58)
--- Master_ModBus/settings.py
+++ Master_ModBus/settings.py
@@ -5,4 +5,11 @@ import minimalmodbus
 
 minimalmodbus.BAUDRATE = 19200
 minimalmodbus.TIMEOUT = 10
-slave_addr = 1
+
+#slave_addr = 1
+port = '/dev/ttyUSB0'
+
+REG_DEBIT = 0
+REG_TEMP = 1
+REG_VANNE_STATE = 2
+REG_DEBIT_STATE = 3
